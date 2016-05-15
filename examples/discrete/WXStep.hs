896985d26e0eb8f48b9b063780272517ac2caac1
Ivan Perez <ivan.perez@keera.co.uk>
2016-05-15 23:01:35 +0100

    Button-driven step counter in WX

---
+++ examples/discrete/WXStep.hs
@@ -0,0 +1,75 @@
+-- Part of this code is taken and adapted from:
+-- https://wiki.haskell.org/WxHaskell/Quick_start#Hello_world_in_wxHaskell
+module Main where
+
+import Prelude hiding ((.))
+import Control.Category
+import Control.Monad
+import Data.IORef
+import Data.MonadicStreamFunction
+import Graphics.UI.WX
+
+main :: IO ()
+main = start $ do
+  f      <- frame      []
+  lenLbl <- staticText f [ text := "0" ]
+  step   <- button     f [ text := "Up" ]
+  quit   <- button     f [ text := "Quit", on command := close f ]
+
+  counter <- newIORef 0
+
+  -- Reactive network
+  let labelMSF  = labelTextSk lenLbl . arr show . ioRefSg counter
+      buttonMSF = ioRefSg counter >>> arr (+1) >>> ioRefSk counter
+
+  let appMSF = (buttonMSF &&& labelMSF) >>> arr (const ())
+
+  hndlr <- pushReactimate_ appMSF 
+  set step [ on command := hndlr ]
+
+  set f [layout := margin 10 (column 5 [ floatCentre (widget lenLbl)
+                                       , floatCentre (widget step)
+                                       , floatCentre (widget quit)
+                                       ] )]
+
+
+-- * Auxiliary definitions
+
+-- ** Adhoc Dunai-WX backend
+textEntryTextSg :: TextCtrl a -> MStream IO String
+textEntryTextSg entry = liftMStreamF_ (get entry text)
+
+labelTextSk :: StaticText a -> MSink IO String
+labelTextSk lbl = liftMStreamF $ setJust lbl text
+  -- (\t -> set lbl [ text := t ])
+
+ioRefSg :: IORef a -> MStream IO a
+ioRefSg ioref = liftMStreamF_ (readIORef ioref)
+
+ioRefSk :: IORef a -> MSink IO a
+ioRefSk ioref = liftMStreamF (writeIORef ioref)
+
+-- ** MSF-related definitions and extensions
+type MSink m a = MStreamF m a ()
+
+-- | Run an MSF on an input sample step by step, using an IORef to store the
+-- continuation.
+pushReactimate :: MStreamF IO a b -> IO (a -> IO b)
+pushReactimate msf = do
+  msfRef <- newIORef msf
+  return $ \a -> do
+              msf' <- readIORef msfRef
+              (b, msf'') <- unMStreamF msf' a
+              writeIORef msfRef msf''
+              return b
+
+-- | Run one step of an MSF on () streams, internally storing the
+-- continuation.
+pushReactimate_ :: MStreamF IO () () -> IO (IO ())
+pushReactimate_ msf = do
+  f <- pushReactimate msf
+  return (void (f ()))
+
+-- ** Auxiliary WX functions
+setJust :: widget -> Attr widget attr -> attr -> IO ()
+setJust c p v = set c [ p := v ]
