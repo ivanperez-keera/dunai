27ec6c0eed34ce389fa7e342658e924807db1a77
Ivan Perez <ivan.perez@keera.co.uk>
2016-05-18 17:51:37 +0100

    Additional aids to test game with saved input

--- examples/classicfrp/GTest.hs
+++ examples/classicfrp/GTest.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE Arrows     #-}
-{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE Arrows            #-}
+{-# LANGUAGE RankNTypes        #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 import           Control.Applicative
 import           Control.Concurrent
 import           Control.Monad
 import           Control.Monad.Identity
 import           Control.Monad.Trans.Reader
+import           Control.Monad.Trans.State
 import           Control.Monad.Trans.Class
 import           Control.Monad.Trans.MStreamF
 import           Data.Maybe
@@ -19,12 +21,12 @@ import           Graphics.UI.SDL.Primitives as SDL
 type Stream m b = SF m () b
 type Signal m a = Stream m a
 
-ballInCirclesAroundMouse :: MouseIO m => Signal m (Int, Int)
+ballInCirclesAroundMouse :: GameMonad m => Signal m (Int, Int)
 ballInCirclesAroundMouse =
   addPair <$> mousePos <*> ballInCircles
 
 -- Mouse position
-mousePos :: MouseIO m => Signal m (Int, Int)
+mousePos :: GameMonad m => Signal m (Int, Int)
 mousePos = liftMStreamF (\() -> lift getMousePos)
 
 -- Ball going around in circles
@@ -48,6 +50,7 @@ main = do
 
 -- Output
 render (px,py) = do
+  print (px, py)
   screen <- SDL.getVideoSurface
 
   white <- SDL.mapRGB (SDL.surfaceGetPixelFormat screen) 0xFF 0xFF 0xFF
@@ -66,14 +69,103 @@ reactimate' sf =
        sense   = arr (const (0.2, ()))
        actuate = liftMStreamF render
 
-class (Functor m, Applicative m, Monad m) => MouseIO m where
+runOnce :: (Monad m, Applicative m) => MStreamF m a b -> a -> m b
+runOnce msf a = head <$> embed msf [a]
+
+runGame :: IO ()
+runGame = do
+  SDL.init [InitEverything]
+  SDL.setVideoMode 800 600 32 [SWSurface]
+  MSF.reactimate $ sense >>> sfIO >>> actuate
+ where sfIO    = runReaderS ballInCirclesAroundMouse
+       sense   = arr (const (0.2, ()))
+       actuate = liftMStreamF render
+
+runDebug :: StateT [PureGameEnv] IO ()
+runDebug =
+  MSF.reactimate $ sense >>> sfIO >>> actuate
+ where sfIO    = runReaderS ballInCirclesAroundMouse
+       sense   = arr (const (0.2, ()))
+       actuate = liftMStreamF (lift . print)
+
+class (Functor m, Applicative m, Monad m) => GameMonad m where
   getMousePos :: m (Int, Int)
 
-instance MouseIO IO where
+instance GameMonad IO where
   getMousePos = do
     pumpEvents
     (x,y,btns) <- SDL.getMouseState
+    putStrLn $ "Mouse position: " ++ show (x,y)
     return (fromIntegral x, fromIntegral y)
 
-instance MouseIO Identity where
+instance GameMonad Identity where
   getMousePos = return (400, 300)
+
+instance (Functor m, Applicative m, Monad m) => GameMonad (ReaderT PureGameEnv m) where
+  getMousePos = pureMousePos <$> ask
+
+data PureGameEnv = PureGameEnv { pureMousePos :: (Int, Int)}
+  
+instance (Functor m, Monad m) => GameMonad (StateT [PureGameEnv] m) where
+  getMousePos = StateT $ \ls -> case ls of
+    []     -> error "End of saved input trace"
+    (p:ps) -> return (pureMousePos p, ps)
+
+testPureEnv :: [ PureGameEnv ]
+testPureEnv = map PureGameEnv
+  [ (418,331)
+  , (496,327)
+  , (613,253)
+  , (616,214)
+  , (500,79) 
+  , (463,59) 
+  , (454,54) 
+  , (437,51) 
+  , (350,63) 
+  , (281,82) 
+  ]
+
+sampleTrace = 
+ [ ((347,128),(266,140))
+ , ((310,149),(252,170))
+ , ((295,183),(252,219))
+ , ((293,237),(252,285))
+ , ((291,307),(252,328))
+ , ((289,353),(297,369))
+ , ((331,398),(584,372))
+ , ((615,404),(737,350))
+ , ((765,385),(737,350))
+ , ((761,388),(737,350))
+ ]
+
+-- Mouse position: (418,331)
+-- (463,335)
+-- Mouse position: (496,327)
+-- (540,336)
+-- Mouse position: (613,253)
+-- (656,266)
+-- Mouse position: (616,214)
+-- (657,232)
+-- Mouse position: (500,79)
+-- (539,101)
+-- Mouse position: (463,59)
+-- (500,84)
+-- Mouse position: (454,54)
+-- (488,83)
+-- Mouse position: (437,51)
+-- (468,83)
+-- Mouse position: (350,63)
+-- (378,98)
+-- Mouse position: (281,82)
+-- (305,120)
+-- 
+-- (463,335)
+-- (540,336)
+-- (656,266)
+-- (657,232)
+-- (539,101)
+-- (500,84)
+-- (488,83)
+-- (468,83)
+-- (378,98)
+-- (305,120)
