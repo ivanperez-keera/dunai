867d651a1a26b4105f353d6f1c380f2050db1985
Ivan Perez <ivan.perez@keera.co.uk>
2017-10-28 14:50:04 +0200

    Moves construct to more general place

--- src/Data/MonadicStreamFunction/Instances/VectorSpace.hs
+++ src/Data/MonadicStreamFunction/Instances/VectorSpace.hs
@@ -2,19 +2,19 @@
 {-# OPTIONS_GHC -fno-warn-orphans #-}
 module Data.MonadicStreamFunction.Instances.VectorSpace where
 
+import Control.Arrow
 import Control.Arrow.Util
 import Data.MonadicStreamFunction.Core
-import Data.MonadicStreamFunction.Util
 import Data.VectorSpace
 
 -- These conflict with Data.VectorSpace.Instances
 instance (Monad m, RModule v) => RModule (MSF m a v) where
   type Groundring (MSF m a v) = Groundring v
   zeroVector   = constantly zeroVector
-  r *^ msf     = elementwise  (r *^) msf
-  negateVector = elementwise  negateVector
+  r *^ msf     = msf >>^ (r *^)
+  negateVector = (>>^ negateVector)
   (^+^)        = elementwise2 (^+^)
   (^-^)        = elementwise2 (^-^)
 
 instance (Monad m, VectorSpace v) => VectorSpace (MSF m a v) where
-  msf ^/ r = elementwise (^/ r) msf
+  msf ^/ r = msf >>^ (^/ r)
