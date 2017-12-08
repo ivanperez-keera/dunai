7405cf1722729c7ae54f455115e3127c97fd82e5
Manuel Bärenz <manuel@enigmage.de>
2017-12-08 21:36:33 +0100

    Fix #81.

--- src/Control/Monad/Trans/MSF/RWS.hs
+++ src/Control/Monad/Trans/MSF/RWS.hs
@@ -1,3 +1,5 @@
+-- | This module combines the wrapping and running functions
+--   for the 'Reader', 'Writer' and 'State' monad layers in a single layer.
 module Control.Monad.Trans.MSF.RWS
   ( module Control.Monad.Trans.MSF.RWS
   , module Control.Monad.Trans.RWS.Strict
@@ -12,8 +14,9 @@ import Data.Monoid
 import Control.Monad.Trans.MSF.GenLift
 import Data.MonadicStreamFunction
 
--- * RWS (Reader-Writer-State) monad
+-- * 'RWS' (Reader-Writer-State) monad
 
+-- | Run the 'RWST' layer by making the state variables explicit.
 runRWSS :: (Functor m, Monad m, Monoid w)
         => MSF (RWST r w s m) a b
         -> MSF m (r, s, a) (w, s, b)
@@ -22,3 +25,15 @@ runRWSS = transS transformInput transformOutput
     transformInput  (_, _, a) = return a
     transformOutput (r, s, _) msfaction = sym <$> runRWST msfaction r s
     sym ((b, msf'), s, w) = ((w, s, b), msf')
+
+-- | Wrap an 'MSF' with explicit state variables in 'RWST' monad.
+rwsS :: (Functor m, Monad m, Monoid w)
+     => MSF m (r, s, a) (w, s, b)
+     -> MSF (RWST r w s m) a b
+rwsS = lifterS wrapRWST
+  where
+    wrapRWST :: Monad m
+             => ((r, s, a) -> m ((w, s, b), c)) -> a -> RWST r w s m (b, c)
+    wrapRWST f a = RWST $ \r s -> do
+      ((w, s', b), c) <- f (r, s, a)
+      return ((b, c), s', w)
