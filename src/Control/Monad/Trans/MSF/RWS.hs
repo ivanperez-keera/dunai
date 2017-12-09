801109c5edd0d028f6ae007c3fa0933dea3d81d9
Manuel Bärenz <manuel@enigmage.de>
2017-12-09 13:10:46 +0100

    Resolves several build warnings

--- src/Control/Monad/Trans/MSF/RWS.hs
+++ src/Control/Monad/Trans/MSF/RWS.hs
@@ -12,7 +12,6 @@ import Data.Monoid
 import Data.Functor ((<$>))
 
 -- Internal
-import Control.Monad.Trans.MSF.GenLift
 import Data.MonadicStreamFunction
 
 -- * 'RWS' (Reader-Writer-State) monad
@@ -22,12 +21,12 @@ import Data.MonadicStreamFunction
 rwsS :: (Functor m, Monad m, Monoid w)
      => MSF m (r, s, a) (w, s, b)
      -> MSF (RWST r w s m) a b
-rwsS = hoistGen $ \f a -> RWST $ \r s -> (\((w, s, b), c) -> ((b, c), s, w))
+rwsS = hoistGen $ \f a -> RWST $ \r s -> (\((w, s', b), c) -> ((b, c), s', w))
    <$> f (r, s, a)
 
 -- | Run the 'RWST' layer by making the state variables explicit.
 runRWSS :: (Functor m, Monad m, Monoid w)
         => MSF (RWST r w s m) a b
         -> MSF m (r, s, a) (w, s, b)
-runRWSS = hoistGen $ \f (r, s, a) -> (\((b, c), s, w) -> ((w, s, b), c))
+runRWSS = hoistGen $ \f (r, s, a) -> (\((b, c), s', w) -> ((w, s', b), c))
       <$> runRWST (f a) r s
