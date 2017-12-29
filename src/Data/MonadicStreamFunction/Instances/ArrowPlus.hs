7de4a8c938c3877ee9eb28259577d22ee5737d1a
Manuel Bärenz <manuel@enigmage.de>
2017-12-29 18:56:05 +0100

    Adds Alternative instance for MSFs with an ArrowPlus instance.
    Since ArrowPlus is to Arrow like Alternative is to Applicative,
    and MSFs have an Applicative instance,
    we should naturally have an Alternative instance.

--- src/Data/MonadicStreamFunction/Instances/ArrowPlus.hs
+++ src/Data/MonadicStreamFunction/Instances/ArrowPlus.hs
@@ -6,9 +6,12 @@
 --   This is only defined for monads that are instances of 'MonadPlus'.
 module Data.MonadicStreamFunction.Instances.ArrowPlus where
 
+-- base
 import Control.Arrow
 import Control.Monad
+import Control.Applicative
 
+-- dunai
 import Data.MonadicStreamFunction.Core
 
 -- | Instance of 'ArrowZero' for Monadic Stream Functions ('MSF').
@@ -20,3 +23,7 @@ instance (Monad m, MonadPlus m) => ArrowZero (MSF m) where
 --   The monad must be an instance of 'MonadPlus'.
 instance (Monad m, MonadPlus m) => ArrowPlus (MSF m) where
   sf1 <+> sf2 = MSF $ \a -> unMSF sf1 a `mplus` unMSF sf2 a
+
+instance (Monad m, MonadPlus m) => Alternative (MSF m a) where
+  empty = zeroArrow
+  (<|>) = (<+>)
