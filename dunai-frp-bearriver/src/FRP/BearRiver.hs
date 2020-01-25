07623ed1e0ed4ab2374cdeff1297e1365a38eac2
Alex Stuart <alex@das.li>
2020-01-25 12:20:04 -0800

    Added `link` combinator, both a Bear River and a Dunai version

--- dunai-frp-bearriver/src/FRP/BearRiver.hs
+++ dunai-frp-bearriver/src/FRP/BearRiver.hs
@@ -18,16 +18,17 @@ import qualified Control.Category                               as Category
 import           Control.Monad                                  (mapM)
 import           Control.Monad.Random
 import           Control.Monad.Trans.Maybe
-import           Control.Monad.Trans.MSF                        hiding (switch)
+import           Control.Monad.Trans.MSF                        hiding (link, switch)
 import qualified Control.Monad.Trans.MSF                        as MSF
 import           Control.Monad.Trans.MSF.Except                 as MSF hiding
-                                                                        (switch)
+                                                                        (link, switch)
 import           Control.Monad.Trans.MSF.List                   (sequenceS,
                                                                  widthFirst)
 import           Control.Monad.Trans.MSF.Random
 import           Data.Functor.Identity
 import           Data.Maybe
-import           Data.MonadicStreamFunction                     as X hiding (reactimate,
+import           Data.MonadicStreamFunction                     as X hiding (link,
+                                                                      reactimate,
                                                                       repeatedly,
                                                                       sum,
                                                                       switch,
@@ -436,6 +437,12 @@ dSwitch sf sfC = MSF $ \a -> do
                        return (b, ct')
     (b, NoEvent) -> return (b, dSwitch ct sfC)
 
+link :: Monad m => SF m a (b, Event c) -> (c -> SF m a (b, Event d)) -> SF m a (b, Event d)
+link sf sfC = MSF $ \a -> do
+  (o, ct) <- unMSF sf a
+  case o of
+    (_, Event c) -> local (const 0) (unMSF (sfC c) a)
+    (b, NoEvent) -> return ((b, NoEvent), link ct sfC)
 
 -- * Parallel composition and switching
 
