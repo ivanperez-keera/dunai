9e4a3b441af4460bf821f7768eccc5d1155cd337
Alex Stuart <alex@das.li>
2020-05-23 11:05:37 -0700

    Changed link to use Either b c instead of (b, Event c)

--- dunai-frp-bearriver/src/FRP/BearRiver.hs
+++ dunai-frp-bearriver/src/FRP/BearRiver.hs
@@ -437,12 +437,22 @@ dSwitch sf sfC = MSF $ \a -> do
                        return (b, ct')
     (b, NoEvent) -> return (b, dSwitch ct sfC)
 
-link :: Monad m => SF m a (b, Event c) -> (c -> SF m a (b, Event d)) -> SF m a (b, Event d)
-link sf sfC = MSF $ \a -> do
+link' :: Monad m => SF m a (b, Event c) -> (c -> SF m a (b, Event d)) -> SF m a (b, Event d)
+link' sf sfC = MSF $ \a -> do
   (o, ct) <- unMSF sf a
   case o of
     (_, Event c) -> local (const 0) (unMSF (sfC c) a)
-    (b, NoEvent) -> return ((b, NoEvent), link ct sfC)
+    (b, NoEvent) -> return ((b, NoEvent), link' ct sfC)
+
+link :: Monad m => SF m a (Either b c) -> (c -> SF m a (Either b d)) -> SF m a (Either b d)
+link sf sfC = MSF $ \a -> do
+  (o, ct) <- unMSF sf a
+  case o of
+    Right c -> local (const 0) (unMSF (sfC c) a)
+    Left b  -> return (Left b, link ct sfC)
+
+
+
 
 -- * Parallel composition and switching
 
