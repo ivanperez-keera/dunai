7dd3f88002b3b75cddb74e22dbfea6ff81a2323d
Miguel Negrao <miguel.negrao@friendlyvirus.org>
2022-03-03 10:27:22 +0000

    Fix BouncingBall.hs

--- dunai-examples/bouncingball/BouncingBall.hs
+++ dunai-examples/bouncingball/BouncingBall.hs
@@ -8,19 +8,30 @@ import FRP.Yampa     as Yampa
 #endif
 
 import Control.Concurrent
+import Data.IORef
+import System.Clock
 import Graphics.UI.SDL            as SDL
 import Graphics.UI.SDL.Primitives as SDL
 
 main = do
    SDL.init [InitEverything]
    SDL.setVideoMode 800 600 32 [SWSurface]
+   t0 <- getTime Monotonic
+   r <- newIORef t0
    reactimate (return ())
-              (\_ -> return (0.01, Just ()))
+              (\_ -> sense r)
               (\_ e -> render e >> return False)
               sf
 
-sf = bouncingBall (100.0 :: Float) 0.0
+sense :: IORef TimeSpec -> IO (Double, Maybe ())
+sense r = do
+  tPrev <- readIORef r
+  t <- getTime Monotonic
+  writeIORef r t
+  let delta = fromIntegral (toNanoSecs (diffTimeSpec t tPrev)) / (1000000000.0 :: Double)
+  return (delta, Just ())
 
+sf = bouncingBall (2.0 :: Double) 0.0
 
 bouncingBall p0 v0 =
   switch (proc (_) -> do
@@ -31,21 +42,11 @@ bouncingBall p0 v0 =
          (\(p,v) -> bouncingBall p (-v))
 
 fallingBall p0 v0 = proc () -> do
-  v <- (v0 +) ^<< integral -< (-99.8)
-  p <- (p0 +) ^<< integral -< v
+  v <- (v0 +) ^<< integral -< (-9.8)
+  p <- (p0 +) ^<< integral <<< averageLast v0 -< v
   returnA -< (p, v)
 
--- bouncingBall p0 v0 =
---   switch (fallingBall p0 v0 >>> (arr id &&& whenS (\(p,v) -> p <= 0 && v < 0)))
---          (\(p,v) -> bouncingBall p (-v))
---
--- fallingBall p0 v0 = proc () -> do
---   v <- (v0 +) ^<< integral -< (-9.8)
---   p <- (p0 +) ^<< integral -< v
---   returnA -< (p, v)
---
--- whenS :: (a -> Bool) -> SF a (Yampa.Event a)
--- whenS p = (((arr p >>> edge) &&& arr id) >>> (arr (uncurry tag)))
+averageLast init = arr (^/2) >>> arr id &&& iPre (zeroVector ^+^ init ^/ 2) >>> arr (uncurry (^+^))
 
 render (p,_) = do
   screen <- SDL.getVideoSurface
@@ -53,7 +54,9 @@ render (p,_) = do
   white <- SDL.mapRGB (SDL.surfaceGetPixelFormat screen) 0xFF 0xFF 0xFF
   SDL.fillRect screen Nothing white
 
-  SDL.filledCircle screen 100 (600 - 100 - round p) 30 (Pixel 0xFF0000FF)
+  SDL.fillRect screen (Just (Rect 0 550 800 600)) (Pixel 0x00000000)
+  let sphereRadius = 30
+  SDL.filledCircle screen 100 (550 - sphereRadius - round (p*100)) sphereRadius (Pixel 0xFF0000FF)
 
   SDL.flip screen
 
