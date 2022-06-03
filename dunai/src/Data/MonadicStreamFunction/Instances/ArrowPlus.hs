2d2960babcfd445f1910bb0c5e687681cf224f15
Ivan Perez <ivan.perez@keera.co.uk>
2022-06-02 22:46:07 -0400

    dunai: Make imports explicit (KSG Haskell 1.3.0 - 3.5). Refs #285.
    Imports should be explicit (aka. no wildcard imports).

--- dunai/src/Data/MonadicStreamFunction/Instances/ArrowPlus.hs
+++ dunai/src/Data/MonadicStreamFunction/Instances/ArrowPlus.hs
@@ -12,13 +12,13 @@
 module Data.MonadicStreamFunction.Instances.ArrowPlus where
 
 -- External imports
-import Control.Applicative
-import Control.Arrow
-import Control.Monad
+import Control.Applicative (Alternative (..))
+import Control.Arrow       (ArrowPlus (..), ArrowZero (..))
+import Control.Monad       (MonadPlus, mplus, mzero)
 
 -- Internal imports
-import Data.MonadicStreamFunction.Core
-import Data.MonadicStreamFunction.InternalCore
+import Data.MonadicStreamFunction.Core         ()
+import Data.MonadicStreamFunction.InternalCore (MSF (MSF, unMSF))
 
 -- | Instance of 'ArrowZero' for Monadic Stream Functions ('MSF').
 --   The monad must be an instance of 'MonadPlus'.
