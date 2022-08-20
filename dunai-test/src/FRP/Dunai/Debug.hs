70fbac77e3669676b0b9c9e367204ebae2fb4d51
Ivan Perez <ivan.perez@keera.co.uk>
2022-08-20 15:35:33 +0000

    dunai-test: Document the module FRP.Dunai.Debug. Refs #306.
    This commit adds the module documentation for FRP.Dunai.Debug and
    documents all top-level definitions.

--- dunai-test/src/FRP/Dunai/Debug.hs
+++ dunai-test/src/FRP/Dunai/Debug.hs
@@ -1,3 +1,9 @@
+-- |
+-- Copyright  : (c) Ivan Perez, 2017
+-- License    : BSD3
+-- Maintainer : ivan.perez@keera.co.uk
+--
+-- Debug FRP networks by inspecting their behaviour inside.
 module FRP.Dunai.Debug where
 
 import Debug.Trace
@@ -6,16 +12,21 @@ import System.IO.Unsafe
 
 -- ** Debugging
 
+-- | Monadic Stream Function that prints the value passing through using
+-- 'trace'.
 traceMSF :: Monad m
          => Show a
         => MSF m a a
 traceMSF = traceMSFWith show
 
+-- | Monadic Stream Function that prints the value passing through using
+-- 'trace', and a customizable 'show' function.
 traceMSFWith :: Monad m
              => (a -> String)
              -> MSF m a a
 traceMSFWith f = arr (\x -> trace (f x) x)
 
+-- | Execute an IO action at every step, and ignore the result.
 traceMSFWithIO :: (a -> IO b)
                -> MSF IO a a
 traceMSFWithIO f = arrM (\x -> f x >> return x)
