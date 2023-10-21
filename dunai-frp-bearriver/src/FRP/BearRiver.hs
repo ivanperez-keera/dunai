5d216125ec7a82dcb54565a4d4cb17ebb3587280
Ivan Perez <ivan.perez@keera.co.uk>
2023-10-21 05:40:55 +0000

    bearriver: Move definitions into FRP.BearRiver.Switches. Refs #382.
    Several definitions are currently being defined in FRP.BearRiver that,
    in Yampa, are instead defined in an internal module FRP.Yampa.Switches.
    To make it easier to port code over from Yampa to bearriver, we decide
    to move those definitions into a matching module FRP.BearRiver.Switches
    on the bearriver side.
    
    The module is currently incomplete. Not all definitions from
    FRP.Yampa.Switches exist in FRP.BearRiver. This commit moves only those
    definitions that currently are being exported by FRP.BearRiver.

--- dunai-frp-bearriver/src/FRP/BearRiver.hs
+++ dunai-frp-bearriver/src/FRP/BearRiver.hs
@@ -21,21 +21,16 @@ module FRP.BearRiver
   where
 
 -- External imports
-#if !MIN_VERSION_base(4,8,0)
-import Control.Applicative   (Applicative (..), (<$>))
-#endif
 import Control.Arrow         as X
 import Control.Monad.Random  (MonadRandom)
 import Data.Functor.Identity (Identity (..))
 import Data.Maybe            (fromMaybe)
-import Data.Traversable      as T
 import Data.VectorSpace      as X
 
 -- Internal imports (dunai)
 import           Control.Monad.Trans.MSF                 hiding (dSwitch,
                                                           switch)
 import qualified Control.Monad.Trans.MSF                 as MSF
-import           Control.Monad.Trans.MSF.List            (sequenceS, widthFirst)
 import           Data.MonadicStreamFunction              (iPre)
 import           Data.MonadicStreamFunction              as X hiding
                                                               (reactimate,
@@ -46,6 +41,7 @@ import           FRP.BearRiver.Arrow                     as X
 import           FRP.BearRiver.Basic                     as X
 import           FRP.BearRiver.Event                     as X
 import           FRP.BearRiver.InternalCore              as X
+import           FRP.BearRiver.Switches                  as X
 
 -- Internal imports (dunai, instances)
 import Data.MonadicStreamFunction.Instances.ArrowLoop () -- not needed, just
@@ -263,140 +259,6 @@ dropEvents :: Monad m => Int -> SF m (Event a) (Event a)
 dropEvents n | n <= 0 = identity
 dropEvents n =
   dSwitch (never &&& identity) (const (NoEvent >-- dropEvents (n - 1)))
-
--- * Switching
-
--- ** Basic switchers
-
--- | Basic switch.
---
--- By default, the first signal function is applied. Whenever the second value
--- in the pair actually is an event, the value carried by the event is used to
--- obtain a new signal function to be applied *at that time and at future
--- times*. Until that happens, the first value in the pair is produced in the
--- output signal.
---
--- Important note: at the time of switching, the second signal function is
--- applied immediately. If that second SF can also switch at time zero, then a
--- double (nested) switch might take place. If the second SF refers to the
--- first one, the switch might take place infinitely many times and never be
--- resolved.
---
--- Remember: The continuation is evaluated strictly at the time
--- of switching!
-switch :: Monad m => SF m a (b, Event c) -> (c -> SF m a b) -> SF m a b
-switch sf sfC = MSF $ \a -> do
-  (o, ct) <- unMSF sf a
-  case o of
-    (_, Event c) -> local (const 0) (unMSF (sfC c) a)
-    (b, NoEvent) -> return (b, switch ct sfC)
-
--- | Switch with delayed observation.
---
--- By default, the first signal function is applied.
---
--- Whenever the second value in the pair actually is an event, the value
--- carried by the event is used to obtain a new signal function to be applied
--- *at future times*.
---
--- Until that happens, the first value in the pair is produced in the output
--- signal.
---
--- Important note: at the time of switching, the second signal function is used
--- immediately, but the current input is fed by it (even though the actual
--- output signal value at time 0 is discarded).
---
--- If that second SF can also switch at time zero, then a double (nested)
--- switch might take place. If the second SF refers to the first one, the
--- switch might take place infinitely many times and never be resolved.
---
--- Remember: The continuation is evaluated strictly at the time
--- of switching!
-dSwitch :: Monad m => SF m a (b, Event c) -> (c -> SF m a b) -> SF m a b
-dSwitch sf sfC = MSF $ \a -> do
-  (o, ct) <- unMSF sf a
-  case o of
-    (b, Event c) -> do (_, ct') <- local (const 0) (unMSF (sfC c) a)
-                       return (b, ct')
-    (b, NoEvent) -> return (b, dSwitch ct sfC)
-
--- * Parallel composition and switching
-
--- ** Parallel composition and switching over collections with broadcasting
-
-#if MIN_VERSION_base(4,8,0)
-parB :: Monad m => [SF m a b] -> SF m a [b]
-#else
-parB :: (Functor m, Monad m) => [SF m a b] -> SF m a [b]
-#endif
--- ^ Spatial parallel composition of a signal function collection. Given a
--- collection of signal functions, it returns a signal function that broadcasts
--- its input signal to every element of the collection, to return a signal
--- carrying a collection of outputs. See 'par'.
---
--- For more information on how parallel composition works, check
--- <https://www.antonycourtney.com/pubs/hw03.pdf>
-parB = widthFirst . sequenceS
-
--- | Decoupled parallel switch with broadcasting (dynamic collection of signal
--- functions spatially composed in parallel). See 'dpSwitch'.
---
--- For more information on how parallel composition works, check
--- <https://www.antonycourtney.com/pubs/hw03.pdf>
-dpSwitchB :: (Functor m, Monad m, Traversable col)
-          => col (SF m a b)
-          -> SF m (a, col b) (Event c)
-          -> (col (SF m a b) -> c -> SF m a (col b))
-          -> SF m a (col b)
-dpSwitchB sfs sfF sfCs = MSF $ \a -> do
-  res <- T.mapM (`unMSF` a) sfs
-  let bs   = fmap fst res
-      sfs' = fmap snd res
-  (e, sfF') <- unMSF sfF (a, bs)
-  ct <- case e of
-          Event c -> snd <$> unMSF (sfCs sfs c) a
-          NoEvent -> return (dpSwitchB sfs' sfF' sfCs)
-  return (bs, ct)
-
--- ** Parallel composition over collections
-
--- | Apply an SF to every element of a list.
---
--- Example:
---
--- >>> embed (parC integral) (deltaEncode 0.1 [[1, 2], [2, 4], [3, 6], [4.0, 8.0 :: Float]])
--- [[0.0,0.0],[0.1,0.2],[0.3,0.6],[0.6,1.2]]
---
--- The number of SFs or expected inputs is determined by the first input
--- list, and not expected to vary over time.
---
--- If more inputs come in a subsequent list, they are ignored.
---
--- >>> embed (parC (arr (+1))) (deltaEncode 0.1 [[0], [1, 1], [3, 4], [6, 7, 8], [1, 1], [0, 0], [1, 9, 8]])
--- [[1],[2],[4],[7],[2],[1],[2]]
---
--- If less inputs come in a subsequent list, an exception is thrown.
---
--- >>> embed (parC (arr (+1))) (deltaEncode 0.1 [[0, 0], [1, 1], [3, 4], [6, 7, 8], [1, 1], [0, 0], [1, 9, 8]])
--- [[1,1],[2,2],[4,5],[7,8],[2,2],[1,1],[2,10]]
-parC :: Monad m => SF m a b -> SF m [a] [b]
-parC = parC0
-  where
-    parC0 :: Monad m => SF m a b -> SF m [a] [b]
-    parC0 sf0 = MSF $ \as -> do
-      os <- T.mapM (\(a, sf) -> unMSF sf a) $
-              zip as (replicate (length as) sf0)
-
-      let bs  = fmap fst os
-          cts = fmap snd os
-      return (bs, parC' cts)
-
-    parC' :: Monad m => [SF m a b] -> SF m [a] [b]
-    parC' sfs = MSF $ \as -> do
-      os <- T.mapM (\(a, sf) -> unMSF sf a) $ zip as sfs
-      let bs  = fmap fst os
-          cts = fmap snd os
-      return (bs, parC' cts)
 
 -- * Discrete to continuous-time signal functions
 
