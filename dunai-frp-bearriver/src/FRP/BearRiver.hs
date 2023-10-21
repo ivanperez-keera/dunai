c2d3d1ab74a55f21a99c4d46abc32b93c40ba9a9
Ivan Perez <ivan.perez@keera.co.uk>
2023-10-21 05:39:20 +0000

    bearriver: Move definitions into FRP.BearRiver.EventS. Refs #382.
    Several definitions are currently being defined in FRP.BearRiver that,
    in Yampa, are instead defined in an internal module FRP.Yampa.EventS. To
    make it easier to port code over from Yampa to bearriver, we decide to
    move those definitions into a matching module FRP.BearRiver.EventS on
    the bearriver side.
    
    The module is currently incomplete. Not all definitions from
    FRP.Yampa.EventS exist in FRP.BearRiver. This commit moves only those
    definitions that currently are being exported by FRP.BearRiver.

--- dunai-frp-bearriver/src/FRP/BearRiver.hs
+++ dunai-frp-bearriver/src/FRP/BearRiver.hs
@@ -28,8 +28,7 @@ import Data.Maybe            (fromMaybe)
 import Data.VectorSpace      as X
 
 -- Internal imports (dunai)
-import           Control.Monad.Trans.MSF                 hiding (dSwitch,
-                                                          switch)
+import           Control.Monad.Trans.MSF                 hiding (dSwitch)
 import qualified Control.Monad.Trans.MSF                 as MSF
 import           Data.MonadicStreamFunction              (iPre)
 import           Data.MonadicStreamFunction              as X hiding
@@ -40,6 +39,7 @@ import           Data.MonadicStreamFunction.InternalCore (MSF (MSF, unMSF))
 import           FRP.BearRiver.Arrow                     as X
 import           FRP.BearRiver.Basic                     as X
 import           FRP.BearRiver.Event                     as X
+import           FRP.BearRiver.EventS                    as X
 import           FRP.BearRiver.InternalCore              as X
 import           FRP.BearRiver.Switches                  as X
 
@@ -83,79 +83,6 @@ sscanPrim f cInit bInit = MSF $ \a -> do
     Nothing       -> return (bInit, sscanPrim f cInit bInit)
     Just (c', b') -> return (b',    sscanPrim f c' b')
 
--- | Event source that never occurs.
-never :: Monad m => SF m a (Event b)
-never = constant NoEvent
-
--- | Event source with a single occurrence at time 0. The value of the event is
--- given by the function argument.
-now :: Monad m => b -> SF m a (Event b)
-now b0 = Event b0 --> never
-
--- | Event source with a single occurrence at or as soon after (local) time /q/
--- as possible.
-after :: Monad m
-      => Time -- ^ The time /q/ after which the event should be produced
-      -> b    -- ^ Value to produce at that time
-      -> SF m a (Event b)
-after q x = feedback q go
-  where
-    go = MSF $ \(_, t) -> do
-           dt <- ask
-           let t' = t - dt
-               e  = if t > 0 && t' < 0 then Event x else NoEvent
-               ct = if t' < 0 then constant (NoEvent, t') else go
-           return ((e, t'), ct)
-
--- | Event source with repeated occurrences with interval q.
---
--- Note: If the interval is too short w.r.t. the sampling intervals, the result
--- will be that events occur at every sample. However, no more than one event
--- results from any sampling interval, thus avoiding an "event backlog" should
--- sampling become more frequent at some later point in time.
-repeatedly :: Monad m => Time -> b -> SF m a (Event b)
-repeatedly q x
-    | q > 0     = afterEach qxs
-    | otherwise = error "bearriver: repeatedly: Non-positive period."
-  where
-    qxs = (q, x):qxs
-
--- | Event source with consecutive occurrences at the given intervals.
---
--- Should more than one event be scheduled to occur in any sampling interval,
--- only the first will in fact occur to avoid an event backlog.
-
--- After all, after, repeatedly etc. are defined in terms of afterEach.
-afterEach :: Monad m => [(Time, b)] -> SF m a (Event b)
-afterEach qxs = afterEachCat qxs >>> arr (fmap head)
-
--- | Event source with consecutive occurrences at the given intervals.
---
--- Should more than one event be scheduled to occur in any sampling interval,
--- the output list will contain all events produced during that interval.
-afterEachCat :: Monad m => [(Time, b)] -> SF m a (Event [b])
-afterEachCat = afterEachCat' 0
-  where
-    afterEachCat' :: Monad m => Time -> [(Time, b)] -> SF m a (Event [b])
-    afterEachCat' _ []  = never
-    afterEachCat' t qxs = MSF $ \_ -> do
-      dt <- ask
-      let (ev, t', qxs') = fireEvents [] (t + dt) qxs
-          ev' = if null ev
-                  then NoEvent
-                  else Event (reverse ev)
-
-      return (ev', afterEachCat' t' qxs')
-
-    fireEvents :: [b] -> Time -> [(Time, b)] -> ([b], Time, [(Time, b)])
-    fireEvents ev t []       = (ev, t, [])
-    fireEvents ev t (qx:qxs)
-        | fst qx < 0   = error "bearriver: afterEachCat: Non-positive period."
-        | overdue >= 0 = fireEvents (snd qx:ev) overdue qxs
-        | otherwise    = (ev, t, qx:qxs)
-      where
-        overdue = t - fst qx
-
 -- * Events
 
 -- | Apply an 'MSF' to every input. Freezes temporarily if the input is
@@ -181,84 +108,6 @@ eventToMaybe = event Nothing Just
 boolToEvent :: Bool -> Event ()
 boolToEvent True  = Event ()
 boolToEvent False = NoEvent
-
--- * Hybrid SF m combinators
-
--- | A rising edge detector. Useful for things like detecting key presses. It
--- is initialised as /up/, meaning that events occurring at time 0 will not be
--- detected.
-edge :: Monad m => SF m Bool (Event ())
-edge = edgeFrom True
-
--- | A rising edge detector that can be initialized as up ('True', meaning that
--- events occurring at time 0 will not be detected) or down ('False', meaning
--- that events occurring at time 0 will be detected).
-iEdge :: Monad m => Bool -> SF m Bool (Event ())
-iEdge = edgeFrom
-
--- | Like 'edge', but parameterized on the tag value.
---
--- From Yampa
-edgeTag :: Monad m => a -> SF m Bool (Event a)
-edgeTag a = edge >>> arr (`tag` a)
-
--- | Edge detector particularized for detecting transitions on a 'Maybe'
--- signal from 'Nothing' to 'Just'.
---
--- From Yampa
-
--- !!! 2005-07-09: To be done or eliminated
--- !!! Maybe could be kept as is, but could be easy to implement directly in
--- !!! terms of sscan?
-edgeJust :: Monad m => SF m (Maybe a) (Event a)
-edgeJust = edgeBy isJustEdge (Just undefined)
-  where
-    isJustEdge Nothing  Nothing     = Nothing
-    isJustEdge Nothing  ma@(Just _) = ma
-    isJustEdge (Just _) (Just _)    = Nothing
-    isJustEdge (Just _) Nothing     = Nothing
-
--- | Edge detector parameterized on the edge detection function and initial
--- state, i.e., the previous input sample. The first argument to the edge
--- detection function is the previous sample, the second the current one.
-edgeBy :: Monad m => (a -> a -> Maybe b) -> a -> SF m a (Event b)
-edgeBy isEdge aPrev = MSF $ \a ->
-  return (maybeToEvent (isEdge aPrev a), edgeBy isEdge a)
-
--- | A rising edge detector that can be initialized as up ('True', meaning that
--- events occurring at time 0 will not be detected) or down ('False', meaning
--- that events occurring at time 0 will be detected).
-edgeFrom :: Monad m => Bool -> SF m Bool (Event())
-edgeFrom prev = MSF $ \a -> do
-  let res | prev      = NoEvent
-          | a         = Event ()
-          | otherwise = NoEvent
-      ct  = edgeFrom a
-  return (res, ct)
-
--- * Stateful event suppression
-
--- | Suppression of initial (at local time 0) event.
-notYet :: Monad m => SF m (Event a) (Event a)
-notYet = feedback False $ arr (\(e, c) ->
-  if c then (e, True) else (NoEvent, True))
-
--- | Suppress all but the first event.
-once :: Monad m => SF m (Event a) (Event a)
-once = takeEvents 1
-
--- | Suppress all but the first n events.
-takeEvents :: Monad m => Int -> SF m (Event a) (Event a)
-takeEvents n | n <= 0 = never
-takeEvents n = dSwitch (arr dup) (const (NoEvent >-- takeEvents (n - 1)))
-
--- | Suppress first n events.
-
--- Here dSwitch or switch does not really matter.
-dropEvents :: Monad m => Int -> SF m (Event a) (Event a)
-dropEvents n | n <= 0 = identity
-dropEvents n =
-  dSwitch (never &&& identity) (const (NoEvent >-- dropEvents (n - 1)))
 
 -- * Discrete to continuous-time signal functions
 
