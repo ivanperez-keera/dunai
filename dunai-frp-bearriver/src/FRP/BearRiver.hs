cc0b69cceb6cad9f29b62839e94d331554ca40be
Ivan Perez <ivan.perez@keera.co.uk>
2024-02-22 09:57:12 +0000

    bearriver: Move definitions into FRP.BearRiver.Hybrid. Refs #400.
    Several definitions are currently being defined in FRP.BearRiver that,
    in Yampa, are instead defined in an internal module FRP.Yampa.Hybrid. To
    make it easier to port code over from Yampa to bearriver, we decide to
    move those definitions into a matching module FRP.BearRiver.Hybrid on
    the bearriver side.
    
    The module is currently incomplete. Not all definitions from
    FRP.Yampa.Hybrid exist in FRP.BearRiver. This commit moves only those
    definitions that currently are being exported by FRP.BearRiver, which
    EventS will depend on.

--- dunai-frp-bearriver/src/FRP/BearRiver.hs
+++ dunai-frp-bearriver/src/FRP/BearRiver.hs
@@ -42,6 +42,7 @@ import           FRP.BearRiver.Conditional               as X
 import           FRP.BearRiver.Delays                    as X
 import           FRP.BearRiver.Event                     as X
 import           FRP.BearRiver.EventS                    as X
+import           FRP.BearRiver.Hybrid                    as X
 import           FRP.BearRiver.Integration               as X
 import           FRP.BearRiver.InternalCore              as X
 import           FRP.BearRiver.Scan                      as X
@@ -51,15 +52,6 @@ import           FRP.BearRiver.Time                      as X
 -- Internal imports (dunai, instances)
 import Data.MonadicStreamFunction.Instances.ArrowLoop () -- not needed, just
                                                          -- re-exported
-
--- * Events
-
--- | Apply an 'MSF' to every input. Freezes temporarily if the input is
--- 'NoEvent', and continues as soon as an 'Event' is received.
-mapEventS :: Monad m => MSF m a b -> MSF m (Event a) (Event b)
-mapEventS msf = proc eventA -> case eventA of
-  Event a -> arr Event <<< msf -< a
-  NoEvent -> returnA           -< NoEvent
 
 -- ** Relation to other types
 
@@ -77,35 +69,6 @@ eventToMaybe = event Nothing Just
 boolToEvent :: Bool -> Event ()
 boolToEvent True  = Event ()
 boolToEvent False = NoEvent
-
--- * Discrete to continuous-time signal functions
-
--- ** Wave-form generation
-
--- | Zero-order hold.
---
--- Converts a discrete-time signal into a continuous-time signal, by holding
--- the last value until it changes in the input signal. The given parameter may
--- be used for time zero, and until the first event occurs in the input signal,
--- so hold is always well-initialized.
---
--- >>> embed (hold 1) (deltaEncode 0.1 [NoEvent, NoEvent, Event 2, NoEvent, Event 3, NoEvent])
--- [1,1,2,2,3,3]
-hold :: Monad m => a -> SF m (Event a) a
-hold a = feedback a $ arr $ \(e, a') ->
-  dup (event a' id e)
-
--- ** Accumulators
-
--- | Accumulator parameterized by the accumulation function.
-accumBy :: Monad m => (b -> a -> b) -> b -> SF m (Event a) (Event b)
-accumBy f b = mapEventS $ accumulateWith (flip f) b
-
--- | Zero-order hold accumulator parameterized by the accumulation function.
-accumHoldBy :: Monad m => (b -> a -> b) -> b -> SF m (Event a) b
-accumHoldBy f b = feedback b $ arr $ \(a, b') ->
-  let b'' = event b' (f b') a
-  in (b'', b'')
 
 -- * State keeping combinators
 
