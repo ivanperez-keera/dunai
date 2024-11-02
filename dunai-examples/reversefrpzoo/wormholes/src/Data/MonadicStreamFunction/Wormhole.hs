d8bf428e7092e9b91ea351456082cda7d83110ab
Ivan Perez <ivan.perez@keera.co.uk>
2024-11-02 10:20:22 +0000

    dunai-examples: Add license header (KSG Haskell 1.3.0 - 3.3). Refs #409.
    Rule: All Haskell files must include a copyright notice in a comment.
    Additional notices at the top may include the full Copyright license, a
    pointer to the license contained in a file, or other conﬁdentiality
    notices and warnings.

--- dunai-examples/reversefrpzoo/wormholes/src/Data/MonadicStreamFunction/Wormhole.hs
+++ dunai-examples/reversefrpzoo/wormholes/src/Data/MonadicStreamFunction/Wormhole.hs
@@ -1,5 +1,10 @@
 {-# LANGUAGE TupleSections #-}
--- | As described by Winograd-Court in his PhD thesis,
+-- |
+-- Copyright  : (c) Ivan Perez and Manuel Baerenz, 2016
+-- License    : BSD3
+-- Maintainer : ivan.perez@keera.co.uk
+--
+-- As described by Winograd-Court in his PhD thesis,
 -- a wormhole consists of a data sink and a data source which are linked
 -- by a synchronisation mechanism.
 -- The aim of this technique is to abstract from the particular mechanism
